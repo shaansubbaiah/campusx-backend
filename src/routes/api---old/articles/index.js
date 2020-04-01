36e23e212eb5a645b435a989be0da7998a2b5e0b
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-04-01 20:33:06 +0530

    Removed old api files

--- src/routes/api---old/articles/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-
-const route = Router()
-
-route.use('/comments', require('./comments'))
-
-module.exports = route
--- src/routes/api---old/profiles.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require('express')
-
-const route = Router()
-
-module.exports = route
