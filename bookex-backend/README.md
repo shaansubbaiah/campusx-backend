db91815ecbccdd856ab04eb52d3a688498f38ac9
Rushali <rushalishetty0@gmail.com>
2020-05-01 19:00:20 +0530

    login

--- bookex-backend/README.md
+++
@@ -1,5 +0,0 @@
-# bookex-backend
-
-use `node src/index.js c` or `npm startc` to delete all tables before running.
-
-use `node src/server.js` or `npm start` to continue with existing tables before running.
--- bookex-frontend/src/components/Login.vue
+++ bookex-frontend/src/components/Login.vue
@@ -15,30 +15,34 @@
                         <md-field class="form-data">
                             <label for="email">EMAIL</label>
                             <md-input name="email" id="email" v-model="user.email" v-validate="'required|email|max:50'"></md-input>
-                            <div v-if="submitted && errors.has('email')">{{errors.first('email')}}</div>
+                            <div v-if="errors.has('email')">{{errors.first('email')}}</div>
                         </md-field>
 
                         <md-field class="form-data">
                             <label for="password">PASSWORD</label>
                             <md-input name="password" id="password" v-model="user.password" v-validate="'required|min:8|max:20'"></md-input>
-                            <div v-if="submitted && errors.has('password')">{{errors.first('password')}}</div>
+                            <div v-if="errors.has('password')">{{errors.first('password')}}</div>
                         </md-field>
 
                         <md-button v-on:click="login" class="md-dense md-raised md-primary">REGISTER</md-button>
                     </div>
                 </form>
+
+                <div v-if="message">{{message}}</div>
+
             </md-dialog-content>
         </md-dialog>
     </div>
 </template>
 
 <script>
-//import http from "../http-common";
+import http from "../http-common";
 
 export default{
     name: "Login",
     data(){
         return{
+            message: "",
             submitted: false,
             user: {
                 email: "",
@@ -49,9 +53,32 @@ export default{
     methods: {
         login(){
             this.submitted = true;
-            this.$validator.validate().then(isValid => {
+            this.$validator.validate().then(async isValid => {
                 if (isValid) {
-                    console.log("valid")
+                    var data = {
+                        email: this.user.email,
+                        password: this.user.password
+                    }
+                    try{
+                        await http
+                        .post("/users/login", data)
+                        .then(response => {
+                            this.message = response.data.message
+                            if(this.message == "Authorization successful"){
+                                this.token = response.data.token
+                                console.log(this.token)
+                            }
+                        })
+                        .catch(e => {
+                        console.log(e);
+                        });
+                    }
+                    catch(err){
+                        console.log(err);
+                    }
+                
+                    this.submitted = true;
+                    //this.$router.push('/');
                 }
             });
         }
